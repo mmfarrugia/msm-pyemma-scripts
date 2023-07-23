bcbe1c6b31070eba633d8b6b67ea8f5374ed926b
mmfarrugia <mfarrugi@nd.edu>
2023-07-23 13:43:51 -0400

    end day

--- 2-reduce-dims/reduce-dims.py
+++ 2-reduce-dims/reduce-dims.py
@@ -3,6 +3,7 @@
 
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+#from msm_pyemma_scripts.plot_helpers import heatmap, annotate_heatmap
 import numpy as np
 import mdshare
 import pyemma
@@ -15,8 +16,129 @@ import multiprocessing
 import time
 import pickle
 import heapq
+import sys
+
+#sys.path.insert(0, '/scratch365/mfarrugi/HMGR/500ns/analysis/msm-pyemma-scripts/')
 
 config.show_progress_bars = False
+
+def heatmap(data, row_labels, col_labels, ax=None,
+            cbar_kw=None, cbarlabel="", **kwargs):
+    """
+    Create a heatmap from a numpy array and two lists of labels.
+
+    Parameters
+    ----------
+    data
+        A 2D numpy array of shape (M, N).
+    row_labels
+        A list or array of length M with the labels for the rows.
+    col_labels
+        A list or array of length N with the labels for the columns.
+    ax
+        A `matplotlib.axes.Axes` instance to which the heatmap is plotted.  If
+        not provided, use current axes or create a new one.  Optional.
+    cbar_kw
+        A dictionary with arguments to `matplotlib.Figure.colorbar`.  Optional.
+    cbarlabel
+        The label for the colorbar.  Optional.
+    **kwargs
+        All other arguments are forwarded to `imshow`.
+    """
+
+    if ax is None:
+        ax = plt.gca()
+
+    if cbar_kw is None:
+        cbar_kw = {}
+
+    # Plot the heatmap
+    im = ax.imshow(data, **kwargs)
+
+    # Create colorbar
+    cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
+    cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
+
+    # Show all ticks and label them with the respective list entries.
+    ax.set_xticks(np.arange(data.shape[1]), labels=col_labels)
+    ax.set_yticks(np.arange(data.shape[0]), labels=row_labels)
+
+    # Let the horizontal axes labeling appear on top.
+    ax.tick_params(top=True, bottom=False,
+                   labeltop=True, labelbottom=False)
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=-30, ha="right",
+             rotation_mode="anchor")
+
+    # Turn spines off and create white grid.
+    ax.spines[:].set_visible(False)
+
+    ax.set_xticks(np.arange(data.shape[1]+1)-.5, minor=True)
+    ax.set_yticks(np.arange(data.shape[0]+1)-.5, minor=True)
+    ax.grid(which="minor", color="w", linestyle='-', linewidth=3)
+    ax.tick_params(which="minor", bottom=False, left=False)
+
+    return im, cbar
+
+
+def annotate_heatmap(im, data=None, valfmt="{x:.2f}",
+                     textcolors=("black", "white"),
+                     threshold=None, **textkw):
+    """
+    A function to annotate a heatmap.
+
+    Parameters
+    ----------
+    im
+        The AxesImage to be labeled.
+    data
+        Data used to annotate.  If None, the image's data is used.  Optional.
+    valfmt
+        The format of the annotations inside the heatmap.  This should either
+        use the string format method, e.g. "$ {x:.2f}", or be a
+        `matplotlib.ticker.Formatter`.  Optional.
+    textcolors
+        A pair of colors.  The first is used for values below a threshold,
+        the second for those above.  Optional.
+    threshold
+        Value in data units according to which the colors from textcolors are
+        applied.  If None (the default) uses the middle of the colormap as
+        separation.  Optional.
+    **kwargs
+        All other arguments are forwarded to each call to `text` used to create
+        the text labels.
+    """
+
+    if not isinstance(data, (list, np.ndarray)):
+        data = im.get_array()
+
+    # Normalize the threshold to the images color range.
+    if threshold is not None:
+        threshold = im.norm(threshold)
+    else:
+        threshold = im.norm(data.max())/2.
+
+    # Set default alignment to center, but allow it to be
+    # overwritten by textkw.
+    kw = dict(horizontalalignment="center",
+              verticalalignment="center")
+    kw.update(textkw)
+
+    # Get the formatter in case a string is supplied
+    if isinstance(valfmt, str):
+        valfmt = matplotlib.ticker.StrMethodFormatter(valfmt)
+
+    # Loop over the data and create a `Text` for each "pixel".
+    # Change the text's color depending on the data.
+    texts = []
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            kw.update(color=textcolors[int(im.norm(data[i, j]) > threshold)])
+            text = im.axes.text(j, i, valfmt(data[i, j], None), **kw)
+            texts.append(text)
+
+    return texts
 
 def mode_ftr_analysis(ftr_corr, prefix:str, data_labels, cutoff0 = 0.9, cutoff=0.75, topN=None, data_label='data'):
     
@@ -28,61 +150,70 @@ def mode_ftr_analysis(ftr_corr, prefix:str, data_labels, cutoff0 = 0.9, cutoff=0
     top_vals = []
     if topN is not None:
         for m in range(num_modes):
-            top_m = map((heapq.nlargest(topN, enumerate(ftr_corr[:,m]), key=lambda x: x[1])))
-            top_indices.append()
+            top_m = list(map(list, zip(*heapq.nlargest(topN, enumerate(ftr_corr[:,m]), key=lambda x: x[1]))))
+            top_indices.append(top_m[0])
+            top_vals.append(top_m[1])
     else:
-        top_indices.append()
+        raise Exception(NotImplementedError())
+        top_pca_indices_1 = np.vstack((np.argwhere(ftr_corr[:,0] > 0.95), np.argwhere(pc_ftr_corr[:,0] < -0.95)))
+        print("top pca indices len "+str(len(top_pca_indices_1))+"\n"+str(top_pca_indices_1))
+        top_pca_vals_1 = pc_ftr_corr[top_pca_indices_1]
 
-    top_pca_indices_1 = np.vstack((np.argwhere(ftr_corr[:,0] > 0.95), np.argwhere(pc_ftr_corr[:,0] < -0.95)))
-    print("top pca indices len "+str(len(top_pca_indices_1))+"\n"+str(top_pca_indices_1))
-    top_pca_vals_1 = pc_ftr_corr[top_pca_indices_1]
+        top_pca_indices_2 = np.vstack((np.argwhere(pc_ftr_corr[:,1] > 0.8), np.argwhere(pc_ftr_corr[:,1] < -0.8)))
+        print("top pca indices len "+str(len(top_pca_indices_2))+"\n"+str(top_pca_indices_2))
+        top_pca_vals_2 = pc_ftr_corr[top_pca_indices_2]
+        #top_indices.append()
 
-    top_pca_indices_2 = np.vstack((np.argwhere(pc_ftr_corr[:,1] > 0.8), np.argwhere(pc_ftr_corr[:,1] < -0.8)))
-    print("top pca indices len "+str(len(top_pca_indices_2))+"\n"+str(top_pca_indices_2))
-    top_pca_vals_2 = pc_ftr_corr[top_pca_indices_2]
-
-    top_pca_vals = np.vstack((top_pca_vals_1, top_pca_vals_2))
-
-    i = ax.imshow(top_pca_vals, cmap='bwr', vmin=-1, vmax=1, aspect='auto')
-
-    ax.set_xticks([0, 1, 2, 3])
-    ax.set_xlabel('PC')
-
-    #ax.set_yticks(top_pca_indices)
-    ax.set_ylabel('torsion')
+    unique_indices = np.unique(top_indices)
+    unique_vals = [top_vals[i, :] for i in unique_indices]
+    unique_labels = data_labels[unique_indices]
 
 
-    fig.colorbar(i);
-    fig.savefig('pca_corr_matrix_ftrs.png')
+    im, cbar = heatmap(unique_vals, unique_labels, range(num_modes), ax=ax, cbarlabel="contribution")
+    texts = annotate_heatmap(im, unique_vals, valfmt="{x:.1f}", textcolors="white")
+    fig.tight_layout()
+    plt.show()
 
-    top_torsions_1 = [torsion_labels[i] for i in top_pca_indices_1[:,0]]
-    top_torsions_2 = [torsion_labels[i] for i in top_pca_indices_2[:,0]]
+    #ax.imshow(top_vals, cmap='bwr', vmin=-1, vmax=1, aspect='auto')
+
+
+    fig.savefig(prefix+'_modes.png')
 
     fig.clear()
     plt.cla()
     plt.clf()
+    print("done mode analysis")
 
 
 
 def run_pca(data, dims, data_label:str, data_labels):
 
-    pca = pyemma.coordinates.pca(torsions, dim=d)
+    if dims < 1:
+        pca = pyemma.coordinates.pca(torsions, var_cutoff=dims)
+    else:
+        pca = pyemma.coordinates.pca(torsions, dim=dims)
     pca_output = pca.get_output()
     if dims < 1:
         n_dims = pca.ndims
     else:
         n_dims = dims
-    prefix = 'pca_d'+str(n_dims)+'_'+data_label+'_'
+    prefix = 'pca_d'+str(n_dims)+'_'+data_label
+    pca.save(prefix+'.model', overwrite=True)
 
     pc_ftr_corr = pca.feature_PC_correlation
     mode_ftr_analysis(pc_ftr_corr, prefix, data_labels, topN = 10, data_label=data_label)
 
+    pca_concatenated = np.concatenate(pca_output)
 
-def run_tica()
+    fig, ax, misc = pyemma.plots.plot_density(pca_concatenated[:,0], pca_concatenated[:,1], cbar=True, alpha=0.1)
+    fig.savefig(prefix+'_density.png')
+    print("done pca")
 
+def run_tica():
+    print()
 
-def run_vamp()
-
+def run_vamp():
+    print()
 
 # DATA VARIABLES
 
@@ -108,7 +239,8 @@ torsions_T = np.asarray(torsions_concatenated).T
 #print("shape "+str(np.shape(torsions_T)))
 #print("lens "+str(len(torsions_T))+" "+str(len(torsions_T[0])))
 
-
+run_pca(torsions[:2], 4, 'torsions', data_labels = torsion_labels)
+run_pca(torsions[:2], 0.9, 'torsions', data_labels = torsion_labels)
 
 # DISTANCES
 
